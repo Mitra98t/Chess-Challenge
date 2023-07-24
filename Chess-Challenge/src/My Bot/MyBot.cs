e74ff8ff92120a7dc26d2363097efcfcc0676a7f
Mitra98t <personalmailfm98@gmail.com>
2023-07-24 18:22:12 +0200

    BIG IMPROVEMENT

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,41 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using ChessChallenge.API;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 public class MyBot : IChessBot
 {
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 100000 };
-
-
+    List<Move> LastMoves = new();
+    double endgameWeight = 0;
     Board board;
     public Move Think(Board board, Timer timer)
     {
         this.board = board;
 
+        // return StartSearch(3);
+        // Console.WriteLine("Endgame Weight: " + endgameWeight);
         //Low on time
-        if (timer.MillisecondsRemaining < 10000)
-        {
-            return startSearch(2);
-        }
-        else if (timer.MillisecondsRemaining < 30000)
-        {
-            return startSearch(3);
-        }
+        int piecesLeft = BitboardHelper.GetNumberOfSetBits(board.AllPiecesBitboard);
+        if (piecesLeft < 4)
+            return StartSearch(5);
+        if (piecesLeft < 8)
+            return StartSearch(4);
         else
-        {
-            return startSearch(4);
-        }
+            return StartSearch(3);
     }
 
-    public Move[] orderMoves(Move[] moves)
+    public Move[] GetOrderMoves(bool onlyCaptures = false)
     {
-        List<int> moveScores = new List<int>();
+        List<(int, Move)> moveScores = new();
+        Move[] moves = board.GetLegalMoves(onlyCaptures);
         foreach (Move move in moves)
         {
             int moveScoreGuess = 0;
             PieceType movePieceType = move.MovePieceType; // Piece i'm moving
             PieceType capturePieceType = board.GetPiece(move.TargetSquare).PieceType; // Piece i'm capturing
+
+            //Encourage Piece Development
+            if (movePieceType == PieceType.Knight || movePieceType == PieceType.Bishop)
+                moveScoreGuess += 80;
+
 
             if (capturePieceType != PieceType.None)
                 moveScoreGuess = 10 * pieceValues[(int)capturePieceType] - pieceValues[(int)movePieceType];
@@ -55,25 +60,17 @@ public class MyBot : IChessBot
             if (movePieceType == PieceType.King)
                 moveScoreGuess -= 10 * pieceValues[(int)movePieceType];
 
-            moveScores.Add(moveScoreGuess);
+            moveScores.Add((moveScoreGuess, move));
         }
 
-        for (int i = 0; i < moves.Length - 2; i++)
-        {
-            for (int j = i + 1; j < moves.Length - 1; j++)
-            {
-                if (moveScores[i] < moveScores[j])
-                {
-                    (moves[j], moves[i]) = (moves[i], moves[j]);
-                    (moveScores[j], moveScores[i]) = (moveScores[i], moveScores[j]);
-                }
-            }
-        }
+        moveScores.Sort((x, y) => y.Item1 - x.Item1);
+        List<Move> movesSorted = new();
 
-        return moves;
+        moveScores.ForEach(x => movesSorted.Add(x.Item2));
+        return movesSorted.ToArray();
     }
 
-    private int evalPosition()
+    private int EvalPosition()
     {
         int value = 0;
 
@@ -85,99 +82,150 @@ public class MyBot : IChessBot
 
             foreach (Piece piece in piece_list)
             {
-                value += multiplier * pieceValues[(int)piece.PieceType];
+                value += multiplier * (pieceValues[(int)piece.PieceType] + MobilityEvaluation(piece));
+                // value += multiplier * pieceValues[(int)piece.PieceType];
             }
         }
+        value += ForceKingToCornerEval();
         return value * (board.IsWhiteToMove ? 1 : -1);
     }
 
-    int searchAllCaptures(int alpha, int beta)
+    int SearchAllCaptures(int alpha, int beta)
     {
-        int evaluation = evalPosition();
+        int evaluation = EvalPosition();
         if (evaluation >= beta)
             return beta;
 
         alpha = Math.Max(alpha, evaluation);
 
         //order move
-        Move[] moves = orderMoves(board.GetLegalMoves(true));
+        Move[] moves = GetOrderMoves(true);
 
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-            evaluation = -searchAllCaptures(-beta, -alpha);
+            evaluation = -SearchAllCaptures(-beta, -alpha);
             board.UndoMove(move);
             if (evaluation >= beta)
-            {
                 return beta;
-            }
             alpha = Math.Max(alpha, evaluation);
         }
 
         return alpha;
     }
 
-    int search(int depth, int alpha, int beta)
+    // TODO make single function incorporating searchAllCaptures
+    int Search(int depth, int alpha, int beta)
     {
         if (depth == 0)
         {
             // Stop searching at the leaf nodes and evaluate the position
-            // return evalPosition();
+            // return EvalPosition();
 
             // Search all captures at the leaf nodes
-            return searchAllCaptures(alpha, beta);
+            return SearchAllCaptures(alpha, beta);
         }
 
-        Move[] moves = orderMoves(board.GetLegalMoves());
+        Move[] moves = GetOrderMoves();
         if (moves.Length == 0)
         {
             if (board.IsInCheck())
-            {
                 return -100000;
-            }
             else
-            {
                 return 0;
-            }
         }
 
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-            int evaluation = -search(depth - 1, -beta, -alpha);
+            int evaluation = -Search(depth - 1, -beta, -alpha);
             board.UndoMove(move);
             if (evaluation >= beta)
-            {
                 return beta;
-            }
             alpha = Math.Max(alpha, evaluation);
         }
         return alpha;
     }
 
 
-    private Move startSearch(int depth)
+    private Move StartSearch(int depth)
     {
-        Move[] moves = orderMoves(board.GetLegalMoves());
-        //random number
+        Move[] moves = GetOrderMoves();
         Move best_move = moves[0];
+        int bestScore = -100000;
 
-        int score = -100000;
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-            int move_score = -search(depth, -100000, 100000);
+            int move_score = -Search(depth, -100000, 100000);
             board.UndoMove(move);
-
-            if (move_score > score)
+            if (move_score > bestScore && !WillBeRepetition(move))
+            // if (move_score > bestScore)
             {
-                score = move_score;
+                bestScore = move_score;
                 best_move = move;
             }
         }
-
+        AddToListMoves(best_move);
         return best_move;
     }
 
+    private int MobilityEvaluation(Piece piece)
+    {
+        int value;
+        switch (piece.PieceType)
+        {
+            case PieceType.Knight:
+                value = BitboardHelper.GetNumberOfSetBits(BitboardHelper.GetKnightAttacks(piece.Square)) * 10;
+                break;
+            case PieceType.Queen:
+            case PieceType.Rook:
+            case PieceType.Bishop:
+                value = BitboardHelper.GetNumberOfSetBits(BitboardHelper.GetSliderAttacks(piece.PieceType, piece.Square, board)) * (20 - pieceValues[(int)piece.PieceType] / 100);
+                break;
+            default:
+                value = 0;
+                break;
+        }
+        return value;
+    }
+
+    private int ForceKingToCornerEval()
+    {
+        endgameWeight = board.PlyCount * 0.001 + (32 - BitboardHelper.GetNumberOfSetBits(board.AllPiecesBitboard)) * 0.05;
+        Square friendlyKingSquare = board.GetKingSquare(board.IsWhiteToMove);
+        Square opponentKingSquare = board.GetKingSquare(!board.IsWhiteToMove);
+
+        // Opponent king away from center
+        // Calculate the distance of the king from the center
+        // int a = Math.Max(3 - opponentKingSquare.File, opponentKingSquare.File - 4);
+        // int b = Math.Max(3 - opponentKingSquare.Rank, opponentKingSquare.Rank - 4);
+        // int dist1 = a + b;
+        // evaluation += dist1;
+        int evaluation = Math.Max(3 - opponentKingSquare.File, opponentKingSquare.File - 4) + Math.Max(3 - opponentKingSquare.Rank, opponentKingSquare.Rank - 4);
+
+        // Friendly king near opponent king
+        // sum of the distance between the two kings
+        // int c = Math.Abs(friendlyKingSquare.File - opponentKingSquare.File);
+        // int d = Math.Abs(friendlyKingSquare.Rank - opponentKingSquare.Rank);
+        // int dist2 = c + d;
+        // evaluation += 14 - dist2;
+        evaluation += 14 - (Math.Abs(friendlyKingSquare.File - opponentKingSquare.File) + Math.Abs(friendlyKingSquare.Rank - opponentKingSquare.Rank));
+
+        return (int)(evaluation * 10 * endgameWeight);
+    }
+
+    private void AddToListMoves(Move move)
+    {
+        LastMoves.Add(move);
+        if (LastMoves.Count > 4)
+            LastMoves.RemoveAt(0);
+
+    }
+
+    private bool WillBeRepetition(Move move)
+    {
+        return LastMoves.Count >= 2 && LastMoves[^2].StartSquare == move.StartSquare && LastMoves[^2].TargetSquare == move.TargetSquare && LastMoves[^3].StartSquare == LastMoves[^1].StartSquare && LastMoves[^3].TargetSquare == LastMoves[^1].TargetSquare;
+    }
 
 }
